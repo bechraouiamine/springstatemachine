d78c56999941205b7018e2a6839fa69a3b55629d
Bechraoui <abechraoui@monceauassurances.com>
2020-10-20 13:31:49 +0200

    Init project and add state machine enums

---
+++ src/main/java/com/bechraoui/springstatemachine/domain/PayementEvent.java
@@ -0,0 +1,8 @@
+package com.bechraoui.springstatemachine.domain;
+
+/**
+ * Created by BECHRAOUI, 20/10/2020
+ */
+public enum PayementEvent {
+    PRE_AUTHORIZE, PRE_AUTH_APPROVED, PRE_AUTH_DECLINED, AUTHORIZE, AUTH_APPROVED, AUTH_DECLINED
+}
---
+++ src/main/java/com/bechraoui/springstatemachine/domain/PaymentState.java
@@ -0,0 +1,8 @@
+package com.bechraoui.springstatemachine.domain;
+
+/**
+ * Created by BECHRAOUI, 20/10/2020
+ */
+public enum PaymentState {
+    NEW, PRE_AUTH, PRE_AUTH_ERROR, AUTH, AUTH_ERROR
+}
