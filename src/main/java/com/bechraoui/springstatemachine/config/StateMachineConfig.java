2f2be34751146a8cd10fa48a9725d3413bf2e677
Bechraoui <abechraoui@monceauassurances.com>
2020-10-20 16:08:13 +0200

    State machine configuration

---
+++ src/main/java/com/bechraoui/springstatemachine/config/StateMachineConfig.java
@@ -0,0 +1,29 @@
+package com.bechraoui.springstatemachine.config;
+
+import com.bechraoui.springstatemachine.domain.PaymentEvent;
+import com.bechraoui.springstatemachine.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+/**
+ * Created by BECHRAOUI, 20/10/2020
+ */
+@Slf4j
+@EnableStateMachineFactory
+@Configuration
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.PRE_AUTH)
+                .end(PaymentState.AUTH_ERROR);
+    }
+}
--- src/main/java/com/bechraoui/springstatemachine/domain/PaymentEvent.java
+++ src/main/java/com/bechraoui/springstatemachine/domain/PaymentEvent.java
@@ -3,6 +3,6 @@ package com.bechraoui.springstatemachine.domain;
 /**
  * Created by BECHRAOUI, 20/10/2020
  */
-public enum PayementEvent {
+public enum PaymentEvent {
     PRE_AUTHORIZE, PRE_AUTH_APPROVED, PRE_AUTH_DECLINED, AUTHORIZE, AUTH_APPROVED, AUTH_DECLINED
 }
